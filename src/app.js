99704c4c54998de9cf3636e31365c876d972197f
Daniela González <danimge94@gmail.com>
2025-06-16 19:31:50 +0000

    Reinicio de Generador de excusas

--- src/app.js
+++ src/app.js
@@ -6,26 +6,13 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 
-let who = ['My mom', 'The bus driver', 'My girlfriend', 'The mailman', 'Kratos'];
-let action = ['ate', 'stole', 'mashed', 'broke'];
-let what = ['my phone', 'my car', 'the Wi-Fi', 'the TV'];
-let when = ['before the class', 'when I was sleeping', 'while I was working', 'during my lunch', 'while I was playing'];
+const who = ['My mom', 'The bus driver', 'My girlfriend', 'The mailman', 'Kratos'];
+const action = ['ate', 'stole', 'mashed', 'broke'];
+const what = ['my phone', 'my car', 'the Wi-Fi', 'the TV'];
+const when = ['before the class', 'when I was sleeping', 'while I was working', 'during my lunch', 'while I was playing'];
 
-function getRandomNumber(a, b) {
-  return Math.floor(Math.random() * (b - a) + a);
-}
+const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
+const getRandom = anyArray => anyArray[getRandomNumber(0, anyArray.length)];
 
-function getRandom(anyArray){
-  let max = anyArray.length;
-  let min = 0;
-  console.log(max, min);
-  let random = getRandomNumber(min, max);
-  return anyArray[random];
-}
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-  document.body.innerHTML = getRandom(who) + " " + getRandom(action) + " " + getRandom(what) + " " + getRandom(when)
-}
+window.onload = () => (console.log("Hello Rigo from the console!"), document.body.innerHTML = `${getRandom(who)} ${getRandom(action)} ${getRandom(what)} ${getRandom(when)}.`);
